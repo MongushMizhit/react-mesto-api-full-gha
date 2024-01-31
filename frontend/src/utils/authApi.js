42da49e4198b00808f04569f2cf35e52d418d0a8
Mizhit Mongush <mongushmmb@yandex.ru>
2024-01-31 13:05:31 +0700

    first review

--- frontend/src/utils/authApi.js
+++ frontend/src/utils/authApi.js
@@ -1,5 +1,4 @@
-//export const BASE_URL = 'https://api.mentor.nomoredomainsmonster.ru';
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://api.mentor.nomoredomainsmonster.ru';
 
 const handleResponse = (res) => {
     if (res.ok) {
